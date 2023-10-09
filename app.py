8018f41834ec1ee302609a05e6f9fa55794b3f87
SirNoods <joshua@ka1schmidt.de>
2023-10-09 18:27:09 +0200

    streamlit tests + initial settlementNameGenerator.py commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="myfirstapp", page_icon=":tada:", layout="wide")
+
+# header section
+
+st.header("This is a test")
+st.subheader("this is another test!")
+st.text("Testing apps can be interesting. ")
+
